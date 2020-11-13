f9b93c8172e05c3d40e51cfded804c05f1376bf6
Yuliana Alvarado <yalvarado924@gmail.com>
2020-11-13 17:30:46 -0600

    commit tree 694437b001b55f8506883f03c4b0f2dd83af1474

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, data|
+  if name == "Freddy Mercury"
+  data.each do |key, value|
+  if key == :favorite_ice_cream_flavors
+    value.delete_if {|ice_cream| ice_cream == "strawberry"}
+  end
+end
+end
+end
 end
